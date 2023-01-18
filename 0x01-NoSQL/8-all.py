e2ed943f907ceb3506ede9f8d7e6118b8f788503
Favvie <favourabangwu@gmail.com>
2023-01-18 04:50:18 +0100

    task 8 update

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
-"""
-This module have a utility function that list all document
-"""
-import pymongo
-
-
-def list_all(mongo_collection):
-    """
-    list all collections
-    """
-    if not mongo_collection:
-        return []
-    return list(mongo_collection.find())
-=======
 """ 8-all.py """
 
 
@@ -21,4 +6,3 @@ def list_all(mongo_collection):
     """ lists all documents in a collection """
     if mongo_collection is not None:
         return mongo_collection.find()
->>>>>>> cf076995d6a71022a7a491972e849f4434e79222
