0d1d47a70111bdb015a7d83c9ea20c16f8f1f151
favvie <favourabangwu@gmail.com>
2023-04-12 16:44:23 +0100

    task 0: exercise

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""writing strings to redis"""
+
+import redis
+import uuid
+
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+
+    def store(self, data: str | bytes | int | float) -> str:
+        key = str(uuid.uuid1())
+        self._redis.set(key, data)
+        return key
