3c7ddbe054a71fa9b4472a49066736d0be19709c
favvie <favourabangwu@gmail.com>
2023-04-12 16:47:57 +0100

    [fix] class function docstring

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,9 +7,11 @@ import uuid
 
 class Cache:
     def __init__(self):
+        """initialization function"""
         self._redis = redis.Redis()
 
     def store(self, data: str | bytes | int | float) -> str:
+        """"""
         key = str(uuid.uuid1())
         self._redis.set(key, data)
         return key
