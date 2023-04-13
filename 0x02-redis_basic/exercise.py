33a27a84bc5afd5645d07062dcdaf6d44b685448
favvie <favourabangwu@gmail.com>
2023-04-13 09:02:21 +0100

    check

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,15 +3,21 @@
 
 import redis
 import uuid
+from typing import Callable
 
 
 class Cache:
     def __init__(self):
         """initialization function"""
         self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: str | bytes | int | float) -> str:
         """"""
         key = str(uuid.uuid1())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable):
+        value = self._redis.get(key)
+        fn(value)
